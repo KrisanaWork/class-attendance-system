8c1813372a38cd000de0fe2938efcca460a7e2e8
krisana <krisana@MacBook-Air-khxng-krisana.local>
2025-09-03 23:20:41 +0700

    update course

--- server/routes/course.js
+++ server/routes/course.js
@@ -5,15 +5,21 @@ import { getCourseByStudentIdQuery } from "../utils/sqlQuery.js";
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const studentId = req.params.id;
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) return res.status(401).json({ error: "No token provided" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const studentId = decoded.id;
 
     const result = await query(getCourseByStudentIdQuery, [studentId]);
 
     res.json(result.rows);
   } catch (err) {
-    console.error("Course fetch by ID error: ", err.message);
+    console.error("Course fetch error: ", err.message);
     res.status(500).json({ error: "Server error" });
   }
 });
